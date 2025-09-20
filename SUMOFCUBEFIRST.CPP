6023b278b6d253225e21b8d34156ff70e38ef138
ADITYA RAJ <adityarajyadav0001@gmail.com>
2025-09-20 22:29:28 +0530

    SUMOFCUBEFIRST.CPP

---
+++ SUMOFCUBEFIRST.CPP
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum;
+    sum=0;
+    cout<<"Enter the value of n ; ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum=(i*i*i)+sum;
+    }
+    cout<<"Sum of cube of first "<< n <<" natural number "<<sum<<endl;
+    return 0;
+}
