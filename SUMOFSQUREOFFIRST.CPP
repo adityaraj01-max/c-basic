4ec25f13ec600400c8c4b434afbfc7dc2618d4ab
ADITYA RAJ <adityarajyadav0001@gmail.com>
2025-09-20 22:29:51 +0530

    SUMOFSQUREOFFIRST.CPP

---
+++ SUMOFSQUREOFFIRST.CPP
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum;
+    sum=0;
+    cout<<"Enter the value of n ; ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        sum=(i*i)+sum;
+    }
+    cout<<"Sum of square of first "<< n <<" natural number "<<sum<<endl;
+    return 0;
+}
