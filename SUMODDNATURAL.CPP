df9646f57c4aa236dcf507eb109fd0b31506ef5e
ADITYA RAJ <adityarajyadav0001@gmail.com>
2025-09-20 22:29:01 +0530

    SUMODDNATURAL.CPP

---
+++ SUMODDNATURAL.CPP
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,sum;
+    sum=0;
+    cout<<"Enter the value of n ; ";
+    cin>>n;
+    for(int i=1;i<=n;i+=2)
+    {
+        sum=sum+i;
+    }
+    cout<<"Sum of first "<< n <<" odd natural number "<<sum<<endl;
+    return 0;
+}
